693b1f067ee816d1c67274311f43bbd13989acd4
Alex_Brand <rzn.russia4912@gmail.com>
2024-11-27 22:56:57 +0300

    page-layout-component 	modified:   src/App.css 	modified:   src/App.tsx

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,10 @@
 const Header = () => {
     return (
         <div>
-            <a href='#'>Home</a>
-            <a href='#'>News</a>
-            <a href='#'>Message</a>
-            <a href='#'>Feed</a>
+            <a href='#'> - Home</a>
+            <a href='#'> - News</a>
+            <a href='#'> - Message</a>
+            <a href='#'> - Feed</a>
         </div>
     );
 }
