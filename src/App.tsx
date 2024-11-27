6a3f6ef145c30bda15c6e4b19aff971fdd29ceb2
Alex_Brand <rzn.russia4912@gmail.com>
2024-11-27 22:59:13 +0300

    page-layout-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
 import './App.css';
-import Footer from './Footer';
-import Header from './Header';
-import Technologies from './Technologies';
 
 const App = () => {
   return (
-    <div>
-      <Header />
-      <Technologies />
-      <Footer />
+    <div className='app-wrapper'>
+      <header className='header'>
+        <img src="https://img.freepik.com/free-vector/bird-colorful-logo-gradient-vector_343694-1365.jpg?size=338&ext=jpg&ga=GA1.1.2008272138.1724716800&semt=ais_hybrid" alt="" />
+      </header>
 
+      <nav className='nav'>
+        <div>Profile</div>
+        <div>Message</div>
+      </nav>
+
+      <div className='content'>
+        Main content
+      </div>
     </div>
   );
 }
