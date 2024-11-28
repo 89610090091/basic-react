fb4c36dc314b1596d0f11aa83efd8ee5aea02611
Alex_Brand <rzn.russia4912@gmail.com>
2024-11-28 23:23:52 +0300

    page-grid_layout

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,32 @@ import './App.css';
 const App = () => {
   return (
     <div className='app-wrapper'>
+
       <header className='header'>
         <img src="https://img.freepik.com/free-vector/bird-colorful-logo-gradient-vector_343694-1365.jpg?size=338&ext=jpg&ga=GA1.1.2008272138.1724716800&semt=ais_hybrid" alt="" />
       </header>
 
       <nav className='nav'>
-        <div>Profile</div>
-        <div>Message</div>
+        <div><a href="">Profile</a></div>
+        <div><a href="">Messages</a></div>
+        <div><a href="">News</a></div>
+        <div><a href="">Music</a></div>
+        <div><a href="">Settings</a></div>
       </nav>
 
       <div className='content'>
-        Main content
+        <div>
+          <img src="https://t4.ftcdn.net/jpg/06/41/10/41/360_F_641104140_lkHlULzw6I1zwaN4I2I3SN3UNHVmawNW.jpg" alt="" />
+        </div>
+        <div>ava+discription</div>
+        <div>My posts</div>
+        <div>New post</div>
+        <div>
+          <div>post 1</div>
+          <div>post 2</div>
+        </div>
       </div>
+
     </div>
   );
 }
